f4e96079b5be20e3080d4e442a0e6e53305cbec9
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-05-02 16:02:19 -0400

    Setup Database

--- GasMan.Data/IdentityModels.cs
+++ GasMan.Data/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace GasMan.Data
 {
@@ -28,6 +30,35 @@ namespace GasMan.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
         }
     }
 }
---
+++ GasMan.Data/RetailPrice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasMan.Data
+{
+    class RetailPrice
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public double US_Average { get; set; }
+
+        [Required]
+        public double Midwest_Average { get; set; }
+
+        public double Speedway_Average { get; set; }
+
+        public double BP_Average { get; set; }
+
+        public double Shell_Average { get; set; }
+
+    }
+}
