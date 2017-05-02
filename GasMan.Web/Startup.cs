95087bfa5f1d1cd2e3514a63e338301c8cf7b4ba
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-05-02 15:42:37 -0400

    Inital User Commit

---
+++ GasMan.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GasMan.Web.Startup))]
+namespace GasMan.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
