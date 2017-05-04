3563c43c9982a87405499b79988fcfdf1200de1d
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-05-04 11:39:03 -0400

    Enabled Migrations, And Added Database

---
+++ GasMan.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace GasMan.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GasMan.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "GasMan.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(GasMan.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ GasMan.Web/Controllers/GasPriceController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GasMan.Data;
+
+namespace GasMan.Web.Controllers
+{
+    public class GasPriceController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: GasPrice
+        public ActionResult Index()
+        {
+            return View(db.GasPrices.ToList());
+        }
+
+        // GET: GasPrice/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RetailPrice retailPrice = db.GasPrices.Find(id);
+            if (retailPrice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(retailPrice);
+        }
+
+        // GET: GasPrice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GasPrice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Date,US_Average,Midwest_Average,Speedway_Average,BP_Average,Shell_Average")] RetailPrice retailPrice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GasPrices.Add(retailPrice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(retailPrice);
+        }
+
+        // GET: GasPrice/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RetailPrice retailPrice = db.GasPrices.Find(id);
+            if (retailPrice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(retailPrice);
+        }
+
+        // POST: GasPrice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Date,US_Average,Midwest_Average,Speedway_Average,BP_Average,Shell_Average")] RetailPrice retailPrice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(retailPrice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(retailPrice);
+        }
+
+        // GET: GasPrice/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RetailPrice retailPrice = db.GasPrices.Find(id);
+            if (retailPrice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(retailPrice);
+        }
+
+        // POST: GasPrice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RetailPrice retailPrice = db.GasPrices.Find(id);
+            db.GasPrices.Remove(retailPrice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
